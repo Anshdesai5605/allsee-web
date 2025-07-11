5cbb82087a2b55301e5c534c25d41df61a22b138
Ansh Desai <anshdesai05@gmail.com>
2025-07-11 13:41:15 +0530

    Add navbar code

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import './navbar.css';
+
 
 const Navbar = () => {
   // Task alloted to Tisha and Ansh
-  return <div>Navbar</div>;
+  return (
+    < nav className="navbar">
+      <div className="logo>">
+        <img src="" alt="Logo"/>
+      </div>
+
+      <ul className="navbar-options">
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Categories</a></li>
+        <li><a href="/">Contact</a></li>
+        <li><button className="enquiry-btn">Enquire</button></li>
+      </ul>
+    </nav>
+  )
 };
 
 export default Navbar;
